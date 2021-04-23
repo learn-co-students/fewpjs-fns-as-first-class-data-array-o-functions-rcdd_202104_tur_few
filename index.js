fe925cccc078035112a3e26ecd7dbf388907011e
Mohamad Ziada <mhd.m.ziada@gmail.com>
2021-04-23 23:10:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 function wakeDog(dogName, dogBreed) {
-    return `Wake ${dogName} the ${dogBreed}`;     
+    return (`Wake ${dogName} the ${dogBreed}`);
 }
-
 function leashDog(dogName, dogBreed) {
-    return `Leash ${dogName} the ${dogBreed}`;
+    return (`Leash ${dogName} the ${dogBreed}`);
   }
-
 
 function walkToPark(dogName, dogBreed) {
-    return `Walk to the park with ${dogName} the ${dogBreed}`;
-  }
+  return (`Walk to the park with ${dogName} the ${dogBreed}`);
+}
 
 function throwFrisbee(dogName, dogBreed) {
-  return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  return (`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 
 function walkHome(dogName, dogBreed) {
-  return `Walk home with ${dogName} the ${dogBreed}`;
-}
-  
-function unleashDog(dogName, dogBreed) {
-  return `Unleash ${dogName} the ${dogBreed}`;
+  return (`Walk home with ${dogName} the ${dogBreed}`);
 }
 
-const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, walkHome, unleashDog]
+function unleashDog(dogName, dogBreed) {
+  return (`Unleash ${dogName} the ${dogBreed}`);
+}
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+
 
 function exerciseDog (dogName, dogBreed) {
-    let activities = [];
-    for (let i = 0; i < length.routine; i++) {
-        activities.push(routine[i](dogName, dogBreed));
-    }
-    return activities;
-}
-
+    const results = [];
+    for (let i = 0; i < routine.length; i++) {
+        results.push(routine[i](dogName, dogBreed));
+     }
+    return results; 
+} 
