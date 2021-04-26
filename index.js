0a92bcc3efdcce8bba2d316c4125480db8f6467f
abduallahB <abduallah.imu2@gmail.com>
2021-04-27 01:28:21 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+function wakeDog(dogName, dogBreed) {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+  }
+
+
+
+function leashDog(dogName, dogBreed){
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName, dogBreed){
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function throwFrisbee(dogName, dogBreed){
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName, dogBreed){
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+function unleashDog(dogName, dogBreed){
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+
+// console.log("Leash Byron the poodle");
+// console.log("Walk to the park with Byron the poodle");
+// console.log("Throw the frisbee for Byron the poodle");
+// console.log("Walk home with Byron the poodle");
+// console.log("Unleash Byron the poodle");
+
+
+
+
+
+
